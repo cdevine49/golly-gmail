cdddc8ad2b60669aa7d1c5c6ef873db560af0ef4
Conor <conorjdevine@gmail.com>
2016-03-27 01:05:27 -0400

    Set up dependencies

---
+++ frontend/gmail.jsx
@@ -0,0 +1,24 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var Gmail = React.createClass({
+
+  render: function() {
+    return (
+      <div>Working</div>
+    );
+  }
+
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  ReactDOM.render(
+    <Gmail />,
+    document.getElementById('root');
+  );
+});
+
+module.exports = Gmail;
