1ae6eed0d0fa99c5b206beb27018b34c6e63cbd1
Conor Devine <conorjdevine@gmail.com>
2016-03-29 11:10:42 -0400

    Add User Sign Up / Sign In

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,47 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+	helper_method :current_user, :current_user_name, :current_user_id, :logged_in?
+
+	private
+	def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+	def logged_in?
+		!!current_user
+	end
+
+  def login!(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+	def logout!
+		current_user.try(:reset_session_token!)
+		session[:session_token] = nil
+	end
+
+
+
+  def current_user_id
+    current_user.id if current_user
+  end
+
+  def current_user_name
+    current_user.username if current_user
+  end
+
+  # before filters
+
+  def require_current_user!
+    redirect_to new_user_url unless current_user
+  end
+
+  def require_no_current_user!
+    redirect_to root_url if current_user
+  end
+
+	def require_signed_in!
+		redirect_to new_session_url unless signed_in?
+	end
 end
