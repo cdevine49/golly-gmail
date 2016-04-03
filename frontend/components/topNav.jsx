c280664e9ce1ce91e9ba7e9505d84cf160106914
Conor Devine <conorjdevine@gmail.com>
2016-04-03 15:03:52 -0400

    More CSS

--- frontend/components/topNav.jsx
+++ frontend/components/topNav.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var ApiUtil = require('../utils/apiUtil');
 var SessionStore = require('../stores/sessionStore');
 var Search = require('./search.jsx');
-
+var Link = require('react-router').Link;
 
 var TopNav = React.createClass({
 
@@ -27,14 +27,57 @@ var TopNav = React.createClass({
 
   render: function() {
     return (
-      <header className='header group'>
-        <nav className='topnav-above group'>
-          <button onClick={ApiUtil.logout}>Logout</button>
-        </nav>
+      <header className='main-header'>
+        <section className='header-top group'>
+        <div className='GollyGmail-logo-header'>GollyGmail</div>
+        <Search />
+        <div className='header-right group'>
+          <span className='header-name'>{SessionStore.currentUser().first_name}</span>
+          <span className='header-badge'>{SessionStore.currentUser().first_name[0].toUpperCase()}</span>
+        </div>
+        <div className='account-box hidden'>
+          <div className='account-info group'>
+            <div className='profile-pic'></div>
+            <div className='user-info'>
+              <span className='name'>{SessionStore.currentUser().first_name + ' ' + SessionStore.currentUser().last_name}</span>
+              <span className='email'>{SessionStore.currentUser().gollygmail}</span>
+            </div>
+          </div>
+          <div className='signin-signup-links group'>
+            <Link className='new-account-link' to={'/signup/'}>Add account</Link>
+            <button className='account-box-logout-button' onClick={ApiUtil.logout}>Logout</button>
+          </div>
+        </div>
+      </section>
+        <nav className='header-navbar'></nav>
       </header>
     );
   }
 
 });
 
+// $(document).ready(function(){
+//       $('.account-box').on('click', function(event){
+//         debugger
+//         console.log('click - form');
+//         event.stopPropagation();
+//       });
+//
+//
+//       $('.header-badge').click(function(event){
+//         debugger
+//         $('.account-box').toggle();
+//         event.stopPropagation();
+//       });
+//
+      // $('html').click(function(event){
+      //   debugger
+      //   console.log('click - body');
+      //   //hide the form if the body is clicked
+      //   $('.account-box').css('display','none');
+      // });
+//
+    // });
+
+// <Link className='my-account-link' to={'/account/'}>My   account</Link>
 module.exports = TopNav;
