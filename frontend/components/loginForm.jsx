3918793122629057afddb289413f48a1c546dd09
Conor Devine <conorjdevine@gmail.com>
2016-04-02 22:14:25 -0400

    Finish Login Page

--- frontend/components/loginForm.jsx
+++ frontend/components/loginForm.jsx
@@ -10,6 +10,7 @@ var LoginForm = React.createClass({
     },
 
   getInitialState: function() {
+    // Add Footer to Login and Sign Up Pages
     return {
       username: '',
       password: ''
@@ -34,18 +35,37 @@ var LoginForm = React.createClass({
 
   render: function() {
     return (
-      <section>
-        <form onSubmit={this._handleSubmit}>
-          <label>Username
-            <input type='text' onChange={this._updateUsername} value={this.state.username} />
-          </label>
+      <section className='login-page'>
+        <h1 id='test' className='GollyGmail-logo-login'>GollyGmail</h1>
+        <h1 className='login-page-main-header'>One Account. Not much of Google.</h1>
+        <h2 className='login-page-sub-header'>Sign in to continue to GollyGmail</h2>
+        <section className='login-form-section'>
+          <form onSubmit={this._handleSubmit} className='login-form'>
+            <label htmlFor='login-username-label' className='login-username-label'></label>
+              <input
+                id='login-username'
+                className='login-username-input'
+                placeholder='Enter your email'
+                type='text'
+                onChange={this._updateUsername}
+                value={this.state.username} />
 
-          <label>Password
-            <input type='password' onChange={this._updatePassword} value={this.state.password} />
-          </label>
-          <button>Log In</button>
-        </form>
-        <Link to={'/signup/'}>Create an Account</Link>
+              <label htmlFor='login-password-label' className='login-password-label'></label>
+              <input
+                id='login-password'
+                className='login-password-input'
+                placeholder='Password'
+                type='password'
+                onChange={this._updatePassword}
+                value={this.state.password} />
+              <button className='login-submit'>Log In</button>
+          </form>
+          <div className='signin-link-and-logo'>
+            <Link className='signup-from-signin-link' to={'/signup/'}>Create account</Link>
+            <p className='signin-bottom-text'>One GollyGmail Account for nothing Google.</p>
+            <div className='signup-multiple-gmail-logo'></div>
+          </div>
+        </section>
       </section>
     );
   }
