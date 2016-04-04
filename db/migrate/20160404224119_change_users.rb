0b799be9a8cfda27b4f11b86768a8e37d0273003
Conor <conorjdevine@gmail.com>
2016-04-04 19:10:43 -0400

    update production.rb for assets on heroku

--- db/migrate/20160404224119_change_users.rb
+++
@@ -1,7 +0,0 @@
-class ChangeUsers < ActiveRecord::Migration
-  def change
-    remove_index :users, :username
-    remove_index :users, :session_token
-    add_index :users, :session_token
-  end
-end
