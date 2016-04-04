84684f31fea8b558d532af027c6bfdb8fd17f45e
Conor Devine <conorjdevine@gmail.com>
2016-04-04 18:47:06 -0400

    update production.rb for assets on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404114146) do
+ActiveRecord::Schema.define(version: 20160404224119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20160404114146) do
     t.string   "secondary_email"
   end
 
-  add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
+  add_index "users", ["session_token"], name: "index_users_on_session_token", using: :btree
 
 end
