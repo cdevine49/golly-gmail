ec140e64ad9b36a3f13236b7a08c92cc9409554e
Conor Devine <conorjdevine@gmail.com>
2016-05-24 17:08:13 -0400

    reverse

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,41 +1,6 @@
 class Email < ActiveRecord::Base
   validates :to, presence: true, :if => :sent
 
-  def self.starred
-    @emails = Email
-    .where("(emails.to = ? AND emails.received = ?) OR (emails.from_email = ? AND emails.received = ?)", current_user.gollygmail, true, current_user.gollygmail, false)
-    .where(starred: true)
-  end
-
-  def self.important
-    @emails = Email
-    .where("(emails.to = ? AND emails.received = ?) OR (emails.from_email = ? AND emails.received = ?)", current_user.gollygmail, true, current_user.gollygmail, false)
-    .where(important: true)
-  end
-
-  def self.outbox
-    @emails = Email
-    .where("emails.from_email = ? AND emails.sent = ? AND emails.received = ?", current_user.gollygmail, true, false)
-  end
-
-  def self.search_results
-    @emails = Email
-    .where("(emails.to = ? AND emails.received = ?) OR emails.from_email = ?", current_user.gollygmail, true, current_user.gollygmail)
-    .search_emails(params[:query])
-  end
-
-  def self.drafts
-    @emails = Email
-    .where("emails.from_email = ?", current_user.gollygmail)
-    .where("emails.sent = ?", false)
-  end
-
-  def self.inbox
-    @emails = Email
-    .where("emails.to = ?", current_user.gollygmail)
-    .where("received = ?", true)
-  end
-
   include PgSearch
   pg_search_scope :search_emails,
                   :against => [:subject, :body, :from_name, :from_email, :image_file_name],
